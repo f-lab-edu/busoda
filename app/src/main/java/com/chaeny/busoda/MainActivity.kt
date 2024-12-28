b71bbc04d133469137f7c32764760dd62ff6feb7
parkcheayeon <ddaany927@gmail.com>
2024-12-28 18:43:13 +0900

    add stop_nav_graph

--- app/src/main/java/com/chaeny/busoda/MainActivity.kt
+++ app/src/main/java/com/chaeny/busoda/MainActivity.kt
@@ -2,16 +2,12 @@ package com.chaeny.busoda
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.chaeny.busoda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
     }
 
 }
