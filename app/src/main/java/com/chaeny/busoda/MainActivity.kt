b0fa3264e87ed6d9c2d5a24bcce7a5ccce477b37
parkcheayeon <ddaany927@gmail.com>
2024-12-28 17:15:06 +0900

    add navigation implementation

--- app/src/main/java/com/chaeny/busoda/MainActivity.kt
+++ app/src/main/java/com/chaeny/busoda/MainActivity.kt
@@ -3,7 +3,6 @@ package com.chaeny.busoda
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chaeny.busoda.databinding.ActivityMainBinding
-import com.chaeny.busoda.stopdetail.StopDetailFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,10 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        supportFragmentManager.beginTransaction()
-            .replace(binding.container.id, StopDetailFragment())
-            .commit()
     }
 
 }
