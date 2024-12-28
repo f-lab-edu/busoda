b7df1f0dcd190cb4ab8f8c538c8c14a0a1b445cf
parkcheayeon <ddaany927@gmail.com>
2024-12-28 15:50:06 +0900

    add FragmentStopDetailBinding

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailFragment.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.chaeny.busoda.stopdetail.databinding.FragmentStopDetailBinding
 
 class StopDetailFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_stop_detail, container, false)
+    ): View {
+        val binding = FragmentStopDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
 }
