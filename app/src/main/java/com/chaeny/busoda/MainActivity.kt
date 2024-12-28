f8d0d3f0317c0c979f5848c4802763fee748aaab
parkcheayeon <ddaany927@gmail.com>
2024-12-28 15:19:28 +0900

    add StopListFragment to MainActivity for testing

--- app/src/main/java/com/chaeny/busoda/MainActivity.kt
+++ app/src/main/java/com/chaeny/busoda/MainActivity.kt
@@ -3,11 +3,20 @@ package com.chaeny.busoda
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.chaeny.busoda.databinding.ActivityMainBinding
+import com.chaeny.busoda.stoplist.StopListFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .replace(binding.container.id, StopListFragment())
+            .commit()
     }
+
 }
