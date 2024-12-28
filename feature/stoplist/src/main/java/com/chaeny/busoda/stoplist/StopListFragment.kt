eb07a262abb34dc7d5054382b5a4808c660173d9
parkcheayeon <ddaany927@gmail.com>
2024-12-29 00:52:18 +0900

    add Navigating across feature modules

--- feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
+++ feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
@@ -5,17 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
+import androidx.navigation.NavDeepLinkRequest
+import androidx.navigation.fragment.findNavController
 import com.chaeny.busoda.stoplist.databinding.FragmentStopListBinding
 
 class StopListFragment : Fragment() {
+
+    private lateinit var binding: FragmentStopListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentStopListBinding.inflate(inflater, container, false)
+        binding = FragmentStopListBinding.inflate(inflater, container, false)
+        initButtonAction()
         return binding.root
+    }
+
+    private fun initButtonAction() {
+        binding.sendLeftButton.setOnClickListener {
+            navigateToStopDetail()
+        }
+
+        binding.sendArriveButton.setOnClickListener {
+            navigateToStopDetail()
+        }
+    }
+
+    private fun navigateToStopDetail() {
+        val request = NavDeepLinkRequest.Builder
+            .fromUri("android-app://com.chaeny.busoda/fragment_stop_detail".toUri())
+            .build()
+        findNavController().navigate(request)
     }
 
 }
