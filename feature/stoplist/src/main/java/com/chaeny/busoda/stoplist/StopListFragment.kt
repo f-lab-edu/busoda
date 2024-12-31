28f9bfb8e4adebb924e1530763c37bcafe1f7484
parkcheayeon <ddaany927@gmail.com>
2024-12-31 23:32:55 +0900

    Add dummy data to StopListFragment for testing RecyclerView

--- feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
+++ feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
@@ -17,7 +17,16 @@ class StopListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStopListBinding.inflate(inflater, container, false)
+        val adapter = StopListAdapter()
+        binding.stopList.adapter = adapter
+        val dummyData = listOf(
+            listOf("16206", "화곡역4번출구", "화곡본동시장"),
+            listOf("16146", "화곡본동시장", "한국폴리텍1.서울강서대학교"),
+            listOf("16143", "한국폴리텍1.서울강서대학교", "우장초등학교"),
+            listOf("16142", "우장초등학교", "강서구청.한국건강관리협회"),
+            listOf("16139", "강서구청.한국건강관리협회", "강서구청사거리.서울디지털대학교")
+        )
+        adapter.submitList(dummyData)
         return binding.root
     }
-
 }
