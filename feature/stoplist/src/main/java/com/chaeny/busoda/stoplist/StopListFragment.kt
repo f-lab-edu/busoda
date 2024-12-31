c57379e0b54d80237ac2a56a671b641376b77042
parkcheayeon <ddaany927@gmail.com>
2024-12-31 20:56:20 +0900

    Remove unused initButtonAction method

--- feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
+++ feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.net.toUri
-import androidx.navigation.NavDeepLinkRequest
-import androidx.navigation.fragment.findNavController
 import com.chaeny.busoda.stoplist.databinding.FragmentStopListBinding
 
 class StopListFragment : Fragment() {
@@ -20,26 +17,7 @@ class StopListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStopListBinding.inflate(inflater, container, false)
-        initButtonAction()
         return binding.root
-    }
-
-    private fun initButtonAction() {
-        binding.sendLeftButton.setOnClickListener {
-            navigateToStopDetail("🚌")
-        }
-
-        binding.sendArriveButton.setOnClickListener {
-            navigateToStopDetail("🚍")
-        }
-    }
-
-    private fun navigateToStopDetail(data: String) {
-        val uri = "android-app://com.chaeny.busoda/fragment_stop_detail?data=$data".toUri()
-        val request = NavDeepLinkRequest.Builder
-            .fromUri(uri)
-            .build()
-        findNavController().navigate(request)
     }
 
 }
