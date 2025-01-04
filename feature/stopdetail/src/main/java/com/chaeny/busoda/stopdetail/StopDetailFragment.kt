ee64608403269dd2d6d834c9ac853b3f196dd31c
parkcheayeon <ddaany927@gmail.com>
2025-01-04 21:14:59 +0900

    Add dummy data to StopDetailFragment for testing RecyclerView

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailFragment.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailFragment.kt
@@ -17,7 +17,20 @@ class StopDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentStopDetailBinding.inflate(inflater, container, false)
+        val adapter = StopDetailAdapter()
+        binding.busList.adapter = adapter
+        val dummyData = listOf(
+            Bus(
+                "604", "화곡본동시장", "2분 38초", "2번째 전", "보통",
+                "16분 18초", "9번째 전", "혼잡"),
+            Bus(
+                "5712", "화곡본동시장", "3분 38초", "3번째 전", "보통",
+                "17분 18초", "10번째 전", "혼잡"),
+            Bus(
+                "652", "화곡역1번출구", "4분 38초", "4번째 전", "보통",
+                "18분 18초", "11번째 전", "혼잡")
+        )
+        adapter.submitList(dummyData)
         return binding.root
     }
-
 }
