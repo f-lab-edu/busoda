14c7e60e86781fe50a06e1d2edf8b3b8cc96d69b
parkcheayeon <ddaany927@gmail.com>
2025-01-09 01:30:49 +0900

    Modify BusArrivalInfo data class

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/BusArrivalInfo.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/BusArrivalInfo.kt
@@ -1,6 +1,6 @@
 package com.chaeny.busoda.stopdetail
 
-data class BusArrivalInfo(
+internal data class BusArrivalInfo(
     val arrivalTime: String,
     val position: String,
     val congestion: String
