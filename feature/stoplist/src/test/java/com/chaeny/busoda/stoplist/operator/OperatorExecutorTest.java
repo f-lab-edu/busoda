9e1982ccaa41183619a24ebfaa96a864310d4646
parkcheayeon <ddaany927@gmail.com>
2025-01-18 15:48:33 +0900

    [test] Add testGetPlusOperator method

--- feature/stoplist/src/test/java/com/chaeny/busoda/stoplist/operator/OperatorExecutorTest.java
+++ feature/stoplist/src/test/java/com/chaeny/busoda/stoplist/operator/OperatorExecutorTest.java
@@ -5,11 +5,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class OperatorExecutorTest {
+    PlusOperator plus = new PlusOperator();
 
     @Test
     public void testPlusOperator() {
-        PlusOperator plus = new PlusOperator();
         int result = plus.operate(2, 2);
         assertEquals(4, result);
+    }
+
+    @Test
+    public void testGetPlusOperator() {
+        String operator = plus.getOperator();
+        assertEquals("+", operator);
     }
 }
