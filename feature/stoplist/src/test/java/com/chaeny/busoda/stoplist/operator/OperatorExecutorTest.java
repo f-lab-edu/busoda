31e9ab1c9d830c70bc7843895d5ad15d91fd0b25
parkcheayeon <ddaany927@gmail.com>
2025-01-18 15:37:30 +0900

    [test] Modify testPlusOperator method - second update

--- feature/stoplist/src/test/java/com/chaeny/busoda/stoplist/operator/OperatorExecutorTest.java
+++ feature/stoplist/src/test/java/com/chaeny/busoda/stoplist/operator/OperatorExecutorTest.java
@@ -1,16 +1,15 @@
 package com.chaeny.busoda.stoplist.operator;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class OperatorExecutorTest {
 
-    OperatorExecutor operatorExecutor = new OperatorExecutor();
-
     @Test
     public void testPlusOperator() {
-        String[] args = {"2", "+", "2"};
-        int result = operatorExecutor.executeOperation(args);
+        PlusOperator plus = new PlusOperator();
+        int result = plus.operate(2, 2);
         assertEquals(4, result);
     }
 }
