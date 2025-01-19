f6c93210d309fadeb1e6db8e0228b4b60b56e11e
parkcheayeon <ddaany927@gmail.com>
2025-01-19 23:32:51 +0900

    [test] Add InstantTaskExecutorRule to StopDetailViewModelTest for LiveData testing

---
+++ feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.chaeny.busoda.stopdetail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import org.junit.Rule
+
+class StopDetailViewModelTest {
+
+    @Rule
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+}
