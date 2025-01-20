d90d73dc3cc80cab11538d1fb71b85c0b88e9900
parkcheayeon <ddaany927@gmail.com>
2025-01-21 01:19:57 +0900

    [test] Add MainCoroutineScopeRule to StopDetailViewModelTest

--- feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
+++ feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
@@ -1,12 +1,14 @@
 package com.chaeny.busoda.stopdetail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.chaeny.busoda.stopdetail.util.MainCoroutineScopeRule
 import org.junit.Rule
 
 class StopDetailViewModelTest {
 
-    @Rule
-    @JvmField
+    @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    @get:Rule
+    val coroutineScope = MainCoroutineScopeRule()
 }
