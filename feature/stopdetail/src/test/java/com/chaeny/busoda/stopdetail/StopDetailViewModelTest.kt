83178d5b5571a7ad50c49625fc4d36e35ec18238
parkcheayeon <ddaany927@gmail.com>
2025-01-23 01:45:22 +0900

    [test] Add whenAsyncDataLoadCalled_UpdatesBusInfos

--- feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
+++ feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
@@ -3,7 +3,12 @@ package com.chaeny.busoda.stopdetail
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.SavedStateHandle
 import com.chaeny.busoda.stopdetail.util.MainCoroutineScopeRule
+import com.chaeny.busoda.stopdetail.util.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceTimeBy
+import kotlinx.coroutines.test.runTest
 import org.junit.Rule
+import org.junit.Before
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -12,12 +17,36 @@ class StopDetailViewModelTest {
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    @ExperimentalCoroutinesApi
     @get:Rule
     val coroutineScope = MainCoroutineScopeRule()
 
+    private lateinit var viewModel: StopDetailViewModel
+
+    @Before
+    fun initViewModel() {
+        val savedStateHandle = SavedStateHandle()
+        savedStateHandle.set(STOP_ID_KEY, TEST_STOP_ID)
+        viewModel = StopDetailViewModel(savedStateHandle)
+    }
+
     @Test
     fun whenInitialized_busInfosIsNull() {
-        val viewModel = StopDetailViewModel(SavedStateHandle())
         assertNull(viewModel.busInfos.value)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun whenAsyncDataLoadCalled_UpdatesBusInfos() = runTest {
+        advanceTimeBy(3500)
+        val busInfos = viewModel.busInfos.getOrAwaitValue()
+        val stopName = busInfos.firstOrNull()?.stopName
+        assertEquals(EXPECTED_STOP_NAME, stopName)
+    }
+
+    companion object {
+        private const val STOP_ID_KEY = "stopId"
+        private const val TEST_STOP_ID = "16206"
+        private const val EXPECTED_STOP_NAME = "화곡역4번출구"
     }
 }
