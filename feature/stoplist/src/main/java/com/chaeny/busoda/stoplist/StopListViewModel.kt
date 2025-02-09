8e65248ac5ea0948f338fca124dd403c20b824bb
parkcheayeon <ddaany927@gmail.com>
2025-02-09 19:11:51 +0900

    Remove BusStop from feature:stoplist and use core:model dependency

--- feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListViewModel.kt
+++ feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.chaeny.busoda.data.repository.BusStopRepository
+import com.chaeny.busoda.model.BusStop
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
