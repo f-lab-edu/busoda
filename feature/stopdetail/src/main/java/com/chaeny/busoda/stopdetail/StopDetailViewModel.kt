60c345966aa5767d95228446a645262d581af0f6
parkcheayeon <ddaany927@gmail.com>
2025-02-09 21:35:55 +0900

    Remove dummy data in StopDetailViewModel and use BusStopDetailRepository

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailViewModel.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailViewModel.kt
@@ -5,13 +5,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.chaeny.busoda.model.BusArrivalInfo
-import com.chaeny.busoda.model.BusInfo
+import com.chaeny.busoda.data.repository.BusStopDetailRepository
 import com.chaeny.busoda.model.BusStopDetail
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-internal class StopDetailViewModel(savedStateHandle: SavedStateHandle) : ViewModel() {
+@HiltViewModel
+internal class StopDetailViewModel @Inject constructor(
+    private val busStopDetailRepository: BusStopDetailRepository,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
     private val dummyData = MutableLiveData<BusStopDetail>()
     private val isLoadingData = MutableLiveData<Boolean>()
@@ -19,111 +24,6 @@ internal class StopDetailViewModel(savedStateHandle: SavedStateHandle) : ViewMod
     val stopDetail: LiveData<BusStopDetail> = dummyData
     val isLoading: LiveData<Boolean> = isLoadingData
     val stopId: LiveData<String> = stopIdData
-
-    private val dummyDataMapCreators: Map<String, BusStopDetail> = mapOf(
-        "16206" to BusStopDetail(
-            "화곡역4번출구", listOf(
-                BusInfo(
-                    "604", "화곡본동시장", listOf(
-                        BusArrivalInfo("2분 38초", "2번째 전", "보통"),
-                        BusArrivalInfo("16분 18초", "9번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "5712", "화곡본동시장", listOf(
-                        BusArrivalInfo("3분 48초", "3번째 전", "여유"),
-                        BusArrivalInfo("17분 19초", "10번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "652", "화곡역1번출구", listOf(
-                        BusArrivalInfo("4분 58초", "4번째 전", "보통"),
-                        BusArrivalInfo("18분 20초", "11번째 전", "매우 혼잡")
-                    )
-                )
-            )
-        ),
-        "16146" to BusStopDetail(
-            "화곡본동시장", listOf(
-                BusInfo(
-                    "604", "한국폴리텍1.서울강서대학교", listOf(
-                        BusArrivalInfo("4분 38초", "2번째 전", "보통"),
-                        BusArrivalInfo("16분 18초", "9번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "5712", "한국폴리텍1.서울강서대학교", listOf(
-                        BusArrivalInfo("3분 48초", "3번째 전", "여유"),
-                        BusArrivalInfo("17분 19초", "10번째 전", "혼잡")
-                    )
-                )
-            )
-        ),
-        "16143" to BusStopDetail(
-            "한국폴리텍1.서울강서대학교", listOf(
-                BusInfo(
-                    "604", "우장초등학교", listOf(
-                        BusArrivalInfo("2분 38초", "2번째 전", "보통"),
-                        BusArrivalInfo("16분 18초", "9번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "5712", "우장초등학교", listOf(
-                        BusArrivalInfo("3분 48초", "3번째 전", "여유"),
-                        BusArrivalInfo("17분 19초", "10번째 전", "혼잡")
-                    )
-                )
-            )
-        ),
-        "16142" to BusStopDetail(
-            "우장초등학교", listOf(
-                BusInfo(
-                    "604", "강서구청.한국건강관리협회", listOf(
-                        BusArrivalInfo("2분 38초", "2번째 전", "보통"),
-                        BusArrivalInfo("16분 18초", "9번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "5712", "강서구청.한국건강관리협회", listOf(
-                        BusArrivalInfo("3분 48초", "3번째 전", "여유"),
-                        BusArrivalInfo("17분 19초", "10번째 전", "혼잡")
-                    )
-                )
-            )
-        ),
-        "16139" to BusStopDetail(
-            "강서구청.한국건강관리협회", listOf(
-                BusInfo(
-                    "604", "강서구청사거리.서울디지털대학교", listOf(
-                        BusArrivalInfo("2분 38초", "2번째 전", "보통"),
-                        BusArrivalInfo("16분 18초", "9번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "5712", "강서구청사거리.서울디지털대학교", listOf(
-                        BusArrivalInfo("3분 48초", "3번째 전", "여유"),
-                        BusArrivalInfo("17분 19초", "10번째 전", "혼잡")
-                    )
-                )
-            )
-        ),
-        "16008" to BusStopDetail(
-            "강서구청사거리.서울디지털대학교", listOf(
-                BusInfo(
-                    "604", "등촌중학교.백석초등학교", listOf(
-                        BusArrivalInfo("2분 38초", "2번째 전", "보통"),
-                        BusArrivalInfo("16분 18초", "9번째 전", "혼잡")
-                    )
-                ),
-                BusInfo(
-                    "5712", "등촌중학교.백석초등학교", listOf(
-                        BusArrivalInfo("3분 48초", "3번째 전", "여유"),
-                        BusArrivalInfo("17분 19초", "10번째 전", "혼잡")
-                    )
-                )
-            )
-        )
-    )
 
     init {
         asyncDataLoad()
@@ -133,13 +33,10 @@ internal class StopDetailViewModel(savedStateHandle: SavedStateHandle) : ViewMod
         isLoadingData.value = true
         viewModelScope.launch {
             delay(3000)
-            dummyData.value = getDummyData()
+            dummyData.value = busStopDetailRepository.getBusStopDetail(stopIdData.value!!)
             isLoadingData.value = false
         }
     }
-
-    private fun getDummyData() =
-        dummyDataMapCreators.get(stopIdData.value) ?: BusStopDetail("", emptyList())
 
     companion object {
         private const val BUS_STOP_ID = "stopId"
