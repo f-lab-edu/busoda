06b0c205572aa5fdfb0dcfa258583d04d8f643b8
parkcheayeon <ddaany927@gmail.com>
2025-02-09 19:30:39 +0900

    Remove dummy data in StopListViewModel and use BusStopRepository

--- feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListViewModel.kt
+++ feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListViewModel.kt
@@ -9,9 +9,9 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-internal class StopListViewModel @Inject constructor(private val repository: BusStopRepository) : ViewModel() {
+internal class StopListViewModel @Inject constructor(busStopRepository: BusStopRepository) : ViewModel() {
 
-    private val dummyData = MutableLiveData(getDummyData())
+    private val dummyData = MutableLiveData(busStopRepository.getBusStops())
     private val _removeCompleted = MutableLiveData<Event<RemoveResult>>()
     private val _busStopClicked = MutableLiveData<Event<String>>()
     val busStops: LiveData<List<BusStop>> = dummyData
@@ -30,13 +30,4 @@ internal class StopListViewModel @Inject constructor(private val repository: Bus
     fun handleBusStopClick(stopId: String) {
         _busStopClicked.value = Event(stopId)
     }
-
-    private fun getDummyData() = listOf(
-        BusStop("16206", "화곡역4번출구", "화곡본동시장"),
-        BusStop("16146", "화곡본동시장", "한국폴리텍1.서울강서대학교"),
-        BusStop("16143", "한국폴리텍1.서울강서대학교", "우장초등학교"),
-        BusStop("16142", "우장초등학교", "강서구청.한국건강관리협회"),
-        BusStop("16139", "강서구청.한국건강관리협회", "강서구청사거리.서울디지털대학교"),
-        BusStop("16008", "강서구청사거리.서울디지털대학교", "등촌중학교.백석초등학교")
-    )
 }
