8c8109ed553af6e5a1eaf8d7a5a8f5d1f56d39bd
parkcheayeon <ddaany927@gmail.com>
2025-02-19 00:06:51 +0900

    [test] Add BusStopDetailRepository mock in StopDetailViewModelTest

--- feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
+++ feature/stopdetail/src/test/java/com/chaeny/busoda/stopdetail/StopDetailViewModelTest.kt
@@ -2,15 +2,21 @@ package com.chaeny.busoda.stopdetail
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.SavedStateHandle
+import com.chaeny.busoda.data.repository.BusStopDetailRepository
+import com.chaeny.busoda.model.BusArrivalInfo
+import com.chaeny.busoda.model.BusInfo
+import com.chaeny.busoda.model.BusStopDetail
 import com.chaeny.busoda.stopdetail.util.MainCoroutineScopeRule
 import com.chaeny.busoda.stopdetail.util.getOrAwaitValue
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.advanceUntilIdle
-import kotlinx.coroutines.test.runTest
-import org.junit.Rule
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.Assert.*
 
 class StopDetailViewModelTest {
 
@@ -22,46 +28,41 @@ class StopDetailViewModelTest {
     val coroutineScope = MainCoroutineScopeRule()
 
     private lateinit var viewModel: StopDetailViewModel
+    private lateinit var repository: BusStopDetailRepository
 
     @Before
     fun initViewModel() {
         val savedStateHandle = SavedStateHandle()
         savedStateHandle.set(STOP_ID_KEY, TEST_STOP_ID)
-        viewModel = StopDetailViewModel(savedStateHandle)
+        repository = mockk {
+            coEvery { getBusStopDetail(TEST_STOP_ID) } returns TEST_STOP_DETAIL
+        }
+        viewModel = StopDetailViewModel(repository, savedStateHandle)
     }
 
     @Test
-    fun `when initialized then stopDetail should be null`() {
-        val stopDetail = viewModel.stopDetail.value
-        assertNull(stopDetail)
-    }
-
-    @ExperimentalCoroutinesApi
-    @Test
-    fun `when asyncDataLoad called then stopName should equal expected value`() = runTest {
-        advanceUntilIdle()
+    fun `when asyncDataLoad called then stopName should equal expected value`() {
         val stopName = viewModel.stopDetail.getOrAwaitValue().stopName
         assertTrue("stopName should not be empty", stopName.isNotEmpty())
         assertEquals(EXPECTED_STOP_NAME, stopName)
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    fun `when stopId invalid then busInfos should be empty`() = runTest {
+    fun `when stopId invalid then busInfos should be empty`() {
         val invalidStopId = "0"
         val savedStateHandle = SavedStateHandle()
         savedStateHandle.set(STOP_ID_KEY, invalidStopId)
-        viewModel = StopDetailViewModel(savedStateHandle)
+        repository = mockk {
+            coEvery { getBusStopDetail(invalidStopId) } returns EMPTY_STOP_DETAIL
+        }
+        viewModel = StopDetailViewModel(repository, savedStateHandle)
 
-        advanceUntilIdle()
         val busInfos = viewModel.stopDetail.getOrAwaitValue().busInfos
         assertTrue(busInfos.isEmpty())
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    fun `when bus arrival info loaded then should contain expected values`() = runTest {
-        advanceUntilIdle()
+    fun `when bus arrival info loaded then should contain expected values`() {
         val busInfos = viewModel.stopDetail.getOrAwaitValue().busInfos
         assertTrue("BusInfos should not be empty", busInfos.isNotEmpty())
 
@@ -71,13 +72,9 @@ class StopDetailViewModelTest {
         assertTrue(arrivalInfo.congestion in CONGESTION_VALUES)
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    fun `when data loading starts and completes then isLoading should be true then false`() = runTest {
-        var isLoading = viewModel.isLoading.getOrAwaitValue()
-        assertTrue(isLoading)
-        advanceUntilIdle()
-        isLoading = viewModel.isLoading.getOrAwaitValue()
+    fun `when data loading completes then isLoading should be false`() {
+        val isLoading = viewModel.isLoading.getOrAwaitValue()
         assertFalse(isLoading)
     }
 
@@ -94,5 +91,15 @@ class StopDetailViewModelTest {
         private const val EXPECTED_TIME_UNIT = "분"
         private const val EXPECTED_POSITION_UNIT = "번째"
         private val CONGESTION_VALUES = listOf("여유", "보통", "혼잡", "매우혼잡")
+        private val EMPTY_STOP_DETAIL = BusStopDetail("", emptyList())
+        private val TEST_STOP_DETAIL = BusStopDetail(
+            "화곡역4번출구", listOf(
+                BusInfo(
+                    "604", "화곡본동시장", listOf(
+                        BusArrivalInfo("2분 38초", "2번째 전", "보통")
+                    )
+                )
+            )
+        )
     }
 }
