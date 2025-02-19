52b060894a634a4f0c108bef79c3a80ac039d188
parkcheayeon <ddaany927@gmail.com>
2025-02-20 01:16:14 +0900

    [test] Refactor to improve StopListViewModelTest test readability

--- feature/stoplist/src/test/java/com/chaeny/busoda/stoplist/StopListViewModelTest.kt
+++ feature/stoplist/src/test/java/com/chaeny/busoda/stoplist/StopListViewModelTest.kt
@@ -7,6 +7,7 @@ import com.chaeny.busoda.model.BusStop
 import com.chaeny.busoda.testing.util.getOrAwaitValue
 import com.chaeny.busoda.testing.util.MainCoroutineScopeRule
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.junit.Assert.*
@@ -28,31 +29,40 @@ class StopListViewModelTest {
     private lateinit var busStopDetailRepository: BusStopDetailRepository
 
     @Before
-    fun initViewModel() {
-        busStopRepository = mockk {
-            coEvery { getBusStops() } returns TEST_BUS_STOPS
-        }
-        busStopDetailRepository = mockk {
-            coEvery { getNextStopName("16206") } returns "화곡본동시장"
-            coEvery { getNextStopName("16146") } returns "한국폴리텍1.서울강서대학교"
-        }
+    fun setup() {
+        busStopRepository = mockk()
+        busStopDetailRepository = mockk()
+    }
+
+    private fun initViewModel() {
+        coEvery { busStopRepository.getBusStops() } returns TEST_BUS_STOPS
+        coEvery { busStopDetailRepository.getNextStopName("16206") } returns "화곡본동시장"
+        coEvery { busStopDetailRepository.getNextStopName("16146") } returns "한국폴리텍1.서울강서대학교"
         viewModel = StopListViewModel(busStopRepository, busStopDetailRepository)
     }
 
     @Test
     fun `when initialized then busStops should include nextStopName`() {
+        initViewModel()
         val busStops = viewModel.busStops.getOrAwaitValue()
         assertEquals(EXPECTED_BUS_STOPS, busStops)
     }
 
     @Test
     fun `when data loading completes then isLoading should be false`() {
+        initViewModel()
+        coVerify {
+            busStopRepository.getBusStops()
+            busStopDetailRepository.getNextStopName(any())
+        }
+
         val isLoading = viewModel.isLoading.getOrAwaitValue()
         assertFalse(isLoading)
     }
 
     @Test
     fun `when stop selected then should emit event with correct expected value`() {
+        initViewModel()
         viewModel.handleBusStopClick(EXPECTED_STOP_ID)
         val clickEvent = viewModel.busStopClicked.getOrAwaitValue()
         assertEquals(EXPECTED_STOP_ID, clickEvent.getContentIfNotHandled())
@@ -60,6 +70,7 @@ class StopListViewModelTest {
 
     @Test
     fun `when last stop removed then should decrease size and emit success`() {
+        initViewModel()
         val expectedSize = viewModel.busStops.getOrAwaitValue().size - 1
         viewModel.removeLastStop()
         val busStopsSize = viewModel.busStops.getOrAwaitValue().size
