f1d414837aacc5d51949379b0897c8eb7e60a37d
parkcheayeon <ddaany927@gmail.com>
2025-02-23 23:33:49 +0900

    Refactor to load service key from local

--- core/data/src/main/java/com/chaeny/busoda/data/network/BusApiService.kt
+++ core/data/src/main/java/com/chaeny/busoda/data/network/BusApiService.kt
@@ -1,12 +1,14 @@
 package com.chaeny.busoda.data.network
 
+import com.chaeny.busoda.data.model.StopDetailResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
+import com.chaeny.busoda.data.BuildConfig
 
 interface BusApiService {
     @GET("getStationByUid")
     suspend fun getStationByUid(
-        @Query("serviceKey") serviceKey: String,
+        @Query("serviceKey", encoded = true) serviceKey: String = BuildConfig.SERVICE_KEY,
         @Query("arsId") arsId: String
-    ): String
+    ): StopDetailResponse
 }
