94c4cb54fb801125cb5c10f509c1d11fd07b9df9
parkcheayeon <ddaany927@gmail.com>
2025-02-26 22:57:23 +0900

    Add congestion mapping and color support

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/ArrivalInfoView.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/ArrivalInfoView.kt
@@ -22,7 +22,10 @@ class ArrivalInfoView @JvmOverloads constructor(
                 textInfoTitle.text = context.getString(R.string.nth_bus, position + 1)
                 textArrivalTime.text = arrivalInfo.arrivalTime
                 textPosition.text = arrivalInfo.position
-                textCongestion.text = arrivalInfo.congestion
+
+                val congestionText = getCongestionText(arrivalInfo.congestion)
+                textCongestion.text = congestionText
+                textCongestion.setTextColor(getCongestionColor(congestionText))
             }
         } else {
             bindEmptyInfo(position)
@@ -35,6 +38,26 @@ class ArrivalInfoView @JvmOverloads constructor(
             textArrivalTime.text = context.getString(R.string.no_info)
             textPosition.text = context.getString(R.string.no_data)
             textCongestion.text = context.getString(R.string.no_data)
+        }
+    }
+
+    private fun getCongestionText(congestion: String?): String {
+        return when (congestion) {
+            "6" -> context.getString(R.string.congestion_very_high)
+            "5" -> context.getString(R.string.congestion_high)
+            "4" -> context.getString(R.string.congestion_medium)
+            "3" -> context.getString(R.string.congestion_low)
+            else -> context.getString(R.string.no_data)
+        }
+    }
+
+    private fun getCongestionColor(congestion: String?): Int {
+        return when (congestion) {
+            context.getString(R.string.congestion_very_high) -> context.getColor(R.color.congestion_very_high)
+            context.getString(R.string.congestion_high) -> context.getColor(R.color.congestion_high)
+            context.getString(R.string.congestion_medium) -> context.getColor(R.color.congestion_medium)
+            context.getString(R.string.congestion_low) -> context.getColor(R.color.congestion_low)
+            else -> context.getColor(R.color.congestion_unknown)
         }
     }
 }
