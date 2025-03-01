1868207c7379202f8284416f2f01db608fd0d200
parkcheayeon <ddaany927@gmail.com>
2025-03-01 22:06:56 +0900

    Add delay to getNextStopName for testing

--- core/data/src/main/java/com/chaeny/busoda/data/repository/DummyBusStopRepository.kt
+++ core/data/src/main/java/com/chaeny/busoda/data/repository/DummyBusStopRepository.kt
@@ -1,15 +1,22 @@
 package com.chaeny.busoda.data.repository
 
 import com.chaeny.busoda.model.BusStop
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 class DummyBusStopRepository @Inject constructor() : BusStopRepository {
-    override suspend fun getBusStops(stopName: String) = listOf(
+
+    private val dummyData: List<BusStop> = listOf(
         BusStop("16206", "화곡역4번출구"),
         BusStop("16146", "화곡본동시장"),
         BusStop("16143", "한국폴리텍1.서울강서대학교"),
         BusStop("16142", "우장초등학교"),
         BusStop("16139", "강서구청.한국건강관리협회"),
-        BusStop("16008", "강서구청사거리.서울디지털대학교")
+        BusStop("16008", "강서구청사거리.서울디지털대")
     )
+
+    override suspend fun getBusStops(stopName: String): List<BusStop> {
+        delay(3000)
+        return dummyData
+    }
 }
