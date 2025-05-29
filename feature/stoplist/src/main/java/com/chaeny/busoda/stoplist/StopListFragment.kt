adab593f19a8c4e51fac34f0a601da9e96d8fc30
parkcheayeon <ddaany927@gmail.com>
2025-05-29 23:45:08 +0900

    Refactor showSoftKeyboard

--- feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
+++ feature/stoplist/src/main/java/com/chaeny/busoda/stoplist/StopListFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.inputmethod.InputMethodManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -26,14 +25,18 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
@@ -117,13 +120,6 @@ class StopListFragment : Fragment() {
         findNavController().navigate(request)
     }
 
-    private fun showSoftKeyboard(view: View) {
-        if (view.requestFocus()) {
-            val imm = requireContext().getSystemService(InputMethodManager::class.java)
-            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
-        }
-    }
-
     @Composable
     private fun StopListContent() {
         val stops by viewModel.busStops.observeAsState(initial = emptyList())
@@ -137,6 +133,7 @@ class StopListFragment : Fragment() {
         modifier: Modifier = Modifier
     ) {
         var keyword by rememberSaveable { mutableStateOf("") }
+        val focusRequester = remember { FocusRequester() }
 
         TextField(
             value = keyword,
@@ -155,9 +152,14 @@ class StopListFragment : Fragment() {
                 )
             },
             modifier = Modifier
+                .focusRequester(focusRequester)
                 .fillMaxWidth()
                 .padding(horizontal = 30.dp).padding(top = 20.dp)
         )
+
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
+        }
     }
 
     @Composable
