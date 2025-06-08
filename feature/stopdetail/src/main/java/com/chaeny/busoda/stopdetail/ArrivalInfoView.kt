7618cf4227ae2644192286f3e81060bc038942dd
parkcheayeon <ddaany927@gmail.com>
2025-06-09 07:16:31 +0900

    Add ArrivalInfo composable

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/ArrivalInfoView.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/ArrivalInfoView.kt
@@ -4,15 +4,15 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.chaeny.busoda.model.BusArrivalInfo
 import com.chaeny.busoda.model.CongestionLevel
 import com.chaeny.busoda.stopdetail.databinding.ArrivalInfoViewBinding
@@ -28,61 +28,9 @@ class ArrivalInfoView @JvmOverloads constructor(
     private var binding = ArrivalInfoViewBinding.inflate(layoutInflater, this)
 
     internal fun bindArrivalInfo(arrivalInfo: BusArrivalInfo?, position: Int, timerFlow: Flow<Int>) {
-        if (arrivalInfo != null) {
-            with(binding) {
-                composeInfoTitle.setContent {
-                    MaterialTheme {
-                        ArrivalTitle(context.getString(R.string.nth_bus, position + 1))
-                    }
-                }
-                composeArrivalTime.setContent {
-                    MaterialTheme {
-                        ArrivalTime("1분 1초 후")
-                    }
-                }
-                composePosition.setContent {
-                    MaterialTheme {
-                        ArrivalPosition(arrivalInfo.position)
-                    }
-                }
-                composeCongestion.setContent {
-                    MaterialTheme {
-                        ArrivalCongestion(
-                            congestion = arrivalInfo.getCongestionText(),
-                            textColor = arrivalInfo.getCongestionColor()
-                        )
-                    }
-                }
-            }
-        } else {
-            bindEmptyInfo(position)
-        }
-    }
-
-    private fun bindEmptyInfo(position: Int) {
-        with(binding) {
-            composeInfoTitle.setContent {
-                MaterialTheme {
-                    ArrivalTitle(context.getString(R.string.nth_bus, position + 1))
-                }
-            }
-            composeArrivalTime.setContent {
-                MaterialTheme {
-                    ArrivalTime(context.getString(R.string.no_info))
-                }
-            }
-            composePosition.setContent {
-                MaterialTheme {
-                    ArrivalPosition(context.getString(R.string.no_data))
-                }
-            }
-            composeCongestion.setContent {
-                MaterialTheme {
-                    ArrivalCongestion(
-                        congestion = context.getString(R.string.no_data),
-                        textColor = context.getColor(R.color.congestion_unknown)
-                    )
-                }
+        binding.composeArrivalInfo.setContent {
+            MaterialTheme {
+                ArrivalInfo(arrivalInfo, position)
             }
         }
     }
@@ -108,64 +56,38 @@ class ArrivalInfoView @JvmOverloads constructor(
     }
 
     @Composable
-    fun ArrivalTitle(
-        title: String,
+    fun ArrivalInfo(
+        arrivalInfo: BusArrivalInfo?,
+        position: Int,
         modifier: Modifier = Modifier
     ) {
-        Text(
-            text = title,
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(start = 5.dp),
-            textAlign = TextAlign.Start,
-            style = MaterialTheme.typography.bodyMedium
-        )
-    }
-
-    @Composable
-    fun ArrivalTime(
-        time: String,
-        modifier: Modifier = Modifier
-    ) {
-        Text(
-            text = time,
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(end = 10.dp),
-            textAlign = TextAlign.End,
-            style = MaterialTheme.typography.titleSmall
-        )
-    }
-
-    @Composable
-    fun ArrivalPosition(
-        position: String,
-        modifier: Modifier = Modifier
-    ) {
-        Text(
-            text = position,
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(end = 10.dp),
-            textAlign = TextAlign.End,
-            style = MaterialTheme.typography.bodyMedium
-        )
-    }
-
-    @Composable
-    fun ArrivalCongestion(
-        congestion: String,
-        textColor: Int,
-        modifier: Modifier = Modifier
-    ) {
-        Text(
-            text = congestion,
-            color = Color(textColor),
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(end = 10.dp),
-            textAlign = TextAlign.End,
-            style = MaterialTheme.typography.bodyMedium
-        )
+        Row(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.nth_bus, position + 1),
+                modifier = Modifier.weight(2f),
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "22분 38초 후",
+                modifier = Modifier.weight(2f),
+                textAlign = TextAlign.End,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Text(
+                text = arrivalInfo?.position ?: stringResource(R.string.no_data),
+                modifier = Modifier.weight(1.5f),
+                textAlign = TextAlign.End,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = arrivalInfo?.getCongestionText() ?: stringResource(R.string.no_data),
+                color = Color(arrivalInfo?.getCongestionColor() ?: android.graphics.Color.GRAY),
+                modifier = Modifier.weight(1.5f),
+                textAlign = TextAlign.End,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
