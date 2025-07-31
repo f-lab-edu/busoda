877a59e5a0c221ac90b8c4dca7861cd8eb25948d
parkcheayeon <ddaany927@gmail.com>
2025-08-01 00:56:26 +0900

    Add compose previews

--- feature/favorites/src/main/java/com/chaeny/busoda/favorites/FavoritesScreen.kt
+++ feature/favorites/src/main/java/com/chaeny/busoda/favorites/FavoritesScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -67,4 +68,22 @@ private fun FavoritesGuide(
             style = MaterialTheme.typography.bodyLarge
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SearchBarPreview() {
+    SearchBar(onClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FavoritesGuidePreview() {
+    FavoritesGuide()
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun FavoritesScreenPreview() {
+    FavoritesScreen(onSearchBarClick = {})
 }
