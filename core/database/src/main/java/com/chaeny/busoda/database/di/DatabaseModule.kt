230766985e72ba9d0071d93d1589959cecd34907
parkcheayeon <ddaany927@gmail.com>
2025-08-26 01:24:15 +0900

    Add DatabaseModule, DaoModule

---
+++ core/database/src/main/java/com/chaeny/busoda/database/di/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.chaeny.busoda.database.di
+
+import android.content.Context
+import androidx.room.Room
+import com.chaeny.busoda.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): AppDatabase = Room.databaseBuilder(
+        context,
+        AppDatabase::class.java,
+        "busoda-database"
+    ).build()
+}
