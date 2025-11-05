f523e1dfddc8f6f280b4cbd27e553d54e276297c
parkcheayeon <ddaany927@gmail.com>
2025-11-05 20:55:16 +0900

    Add system bars padding to FavoritesScreen

--- feature/favorites/src/main/java/com/chaeny/busoda/favorites/FavoritesScreen.kt
+++ feature/favorites/src/main/java/com/chaeny/busoda/favorites/FavoritesScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -50,7 +51,11 @@ fun FavoritesScreen(
 
     CollectEffect(viewModel, navigateToStopDetail)
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+    ) {
         SearchBar(navigateToStopList = navigateToStopList)
         if (uiState.favorites.isEmpty()) {
             FavoritesGuide()
