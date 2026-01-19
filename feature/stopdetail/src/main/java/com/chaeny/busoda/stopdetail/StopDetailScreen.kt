268bd89e861ab8a1023c33b30dde3de58245a83c
parkcheayeon <ddaany927@gmail.com>
2026-01-20 01:42:24 +0900

    Add system bars padding to StopDetailScreen

--- feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailScreen.kt
+++ feature/stopdetail/src/main/java/com/chaeny/busoda/stopdetail/StopDetailScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
@@ -121,6 +122,7 @@ private fun StopDetailContent(
     Box(
         modifier = modifier
             .fillMaxSize()
+            .systemBarsPadding()
             .padding(top = 20.dp)
     ) {
         IconButton(
