f9f69d98ccfe19ee18f29cd4e00dd990d3a24ba8
parkcheayeon <ddaany927@gmail.com>
2026-02-08 21:41:20 +0900

    Add debug logs to track nearby stops API calls

--- feature/nearbystops/src/main/java/com/chaeny/busoda/nearbystops/NearbystopsViewModel.kt
+++ feature/nearbystops/src/main/java/com/chaeny/busoda/nearbystops/NearbystopsViewModel.kt
@@ -1,5 +1,6 @@
 package com.chaeny.busoda.nearbystops
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
 import com.chaeny.busoda.data.repository.NearbyBusStopsRepository
 import com.chaeny.busoda.model.BusStopPosition
@@ -26,10 +27,10 @@ internal class NearbystopsViewModel @Inject constructor(
             }
             is NearbystopsIntent.UpdateLocation -> {
                 setState { copy(currentLocation = intent.location) }
-                loadNearbyBusStops(intent.location.latitude, intent.location.longitude)
+                loadNearbyBusStops(intent.location.latitude, intent.location.longitude, source = "GPS")
             }
             is NearbystopsIntent.LoadNearbyStops -> {
-                loadNearbyBusStops(intent.location.latitude, intent.location.longitude)
+                loadNearbyBusStops(intent.location.latitude, intent.location.longitude, source = "Camera")
             }
             is NearbystopsIntent.ClickBusStop -> {
                 postSideEffect(NearbystopsEffect.NavigateToStopDetail(intent.stopId))
@@ -37,14 +38,22 @@ internal class NearbystopsViewModel @Inject constructor(
         }
     }
 
-    private fun loadNearbyBusStops(latitude: Double, longitude: Double) {
+    private fun loadNearbyBusStops(latitude: Double, longitude: Double, source: String) {
         viewModelScope.launch {
+            val isSeoulCityHall = latitude in 37.5665..37.5666 && longitude in 126.9779..126.9781
+            val location = if (isSeoulCityHall) "서울시청" else "현재위치"
+
+            Log.d(TAG, "[$source] API 호출: $location")
+
             val busStops = nearbyBusStopsRepository.getNearbyBusStops(latitude, longitude, DEFAULT_RADIUS)
             setState { copy(busStops = busStops) }
+
+            Log.d(TAG, "[$source] API 완료: 정류소 ${busStops.size}개")
         }
     }
 
     companion object {
+        private const val TAG = "NearbystopsViewModel"
         private const val DEFAULT_RADIUS = 500
     }
 }
