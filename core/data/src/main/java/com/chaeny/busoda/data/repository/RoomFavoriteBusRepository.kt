900e26e229ea528ccd9b844fd6ede910c382a38b
parkcheayeon <ddaany927@gmail.com>
2026-03-05 23:13:50 +0900

    Add stop favorite automatically when adding bus

--- core/data/src/main/java/com/chaeny/busoda/data/repository/RoomFavoriteBusRepository.kt
+++ core/data/src/main/java/com/chaeny/busoda/data/repository/RoomFavoriteBusRepository.kt
@@ -1,14 +1,17 @@
 package com.chaeny.busoda.data.repository
 
 import com.chaeny.busoda.database.dao.FavoriteBusDao
+import com.chaeny.busoda.database.dao.FavoriteStopDao
 import com.chaeny.busoda.database.model.FavoriteBus
+import com.chaeny.busoda.database.model.FavoriteStop
 import com.chaeny.busoda.model.FavoriteBusItem
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RoomFavoriteBusRepository @Inject constructor(
-    private val favoriteBusDao: FavoriteBusDao
+    private val favoriteBusDao: FavoriteBusDao,
+    private val favoriteStopDao: FavoriteStopDao
 ) : FavoriteBusRepository {
 
     override suspend fun addFavoriteBus(stopId: String, stopName: String, busNumber: String, nextStopName: String) {
@@ -17,6 +20,13 @@ class RoomFavoriteBusRepository @Inject constructor(
                 stopId = stopId,
                 stopName = stopName,
                 busNumber = busNumber,
+                nextStopName = nextStopName
+            )
+        )
+        favoriteStopDao.insertFavorite(
+            FavoriteStop(
+                stopId = stopId,
+                stopName = stopName,
                 nextStopName = nextStopName
             )
         )
