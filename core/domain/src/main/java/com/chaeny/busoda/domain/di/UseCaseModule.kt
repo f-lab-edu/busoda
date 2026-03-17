20b45375e11fcebd0a1c74379e47ae9fb96126fa
parkcheayeon <ddaany927@gmail.com>
2026-03-18 00:01:46 +0900

    Add DeleteFavoriteStopUseCase binding to UseCaseModule

--- core/domain/src/main/java/com/chaeny/busoda/domain/di/UseCaseModule.kt
+++ core/domain/src/main/java/com/chaeny/busoda/domain/di/UseCaseModule.kt
@@ -2,6 +2,8 @@ package com.chaeny.busoda.domain.di
 
 import com.chaeny.busoda.domain.usecase.AddFavoriteBusUseCase
 import com.chaeny.busoda.domain.usecase.DefaultAddFavoriteBusUseCase
+import com.chaeny.busoda.domain.usecase.DefaultDeleteFavoriteStopUseCase
+import com.chaeny.busoda.domain.usecase.DeleteFavoriteStopUseCase
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,8 @@ internal abstract class UseCaseModule {
     @Binds
     @Singleton
     abstract fun bindAddFavoriteBusUseCase(implementation: DefaultAddFavoriteBusUseCase): AddFavoriteBusUseCase
+
+    @Binds
+    @Singleton
+    abstract fun bindDeleteFavoriteStopUseCase(implementation: DefaultDeleteFavoriteStopUseCase): DeleteFavoriteStopUseCase
 }
