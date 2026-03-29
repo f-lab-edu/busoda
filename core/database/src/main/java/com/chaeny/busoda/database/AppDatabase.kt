dbc3be808bd000be5d2601e9e205e37eef8078c6
parkcheayeon <ddaany927@gmail.com>
2026-03-29 21:11:46 +0900

    Add Room Gradle Plugin and export version 2 schema

--- core/database/src/main/java/com/chaeny/busoda/database/AppDatabase.kt
+++ core/database/src/main/java/com/chaeny/busoda/database/AppDatabase.kt
@@ -7,7 +7,7 @@ import com.chaeny.busoda.database.dao.FavoriteStopDao
 import com.chaeny.busoda.database.model.FavoriteBus
 import com.chaeny.busoda.database.model.FavoriteStop
 
-@Database(entities = [FavoriteStop::class, FavoriteBus::class], version = 2, exportSchema = false)
+@Database(entities = [FavoriteStop::class, FavoriteBus::class], version = 2, exportSchema = true)
 internal abstract class AppDatabase : RoomDatabase() {
     abstract fun favoriteStopDao(): FavoriteStopDao
     abstract fun favoriteBusDao(): FavoriteBusDao
